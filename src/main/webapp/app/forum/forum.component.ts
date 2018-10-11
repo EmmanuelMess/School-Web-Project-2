d7b3b87922091df523aaa63b255fc5203fbcc7e3
Emmanuel M <emmanuelbendavid@gmail.com>
2018-10-10 23:05:32 -0300

    Load threads and correct page name

--- src/main/webapp/app/forum/forum.component.ts
+++ src/main/webapp/app/forum/forum.component.ts
@@ -1,19 +1,135 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { MessageComponent } from 'app/forum/message/message.component';
+import { IThread, Thread } from 'app/shared/model/thread.model';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { ThreadService } from 'app/entities/thread';
+import { JhiAlertService, JhiEventManager, JhiParseLinks } from 'ng-jhipster';
+import { Principal } from 'app/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ITEMS_PER_PAGE } from 'app/shared';
 
 @Component({
     selector: 'jhi-forum',
     templateUrl: './forum.component.html',
     styleUrls: ['forum.scss']
 })
-export class ForumComponent {
-    weapons = Array<Thread>(50);
+export class ForumComponent implements OnInit {
+    currentAccount: any;
+    threads: Thread[];
+    error: any;
+    success: any;
+    eventSubscriber: Subscription;
+    routeData: any;
+    links: any;
+    totalItems: any;
+    queryCount: any;
+    itemsPerPage: any;
+    page: any;
+    predicate: any;
+    previousPage: any;
+    reverse: any;
 
-    constructor(private dialog: MatDialog) {
-        for (let i = 0; i < 50; i++) {
-            this.weapons[i] = new Thread(i.toString(), 'Rithmio', 'At Rithmio I...');
+    constructor(
+        private dialog: MatDialog,
+        private threadService: ThreadService,
+        private parseLinks: JhiParseLinks,
+        private jhiAlertService: JhiAlertService,
+        private principal: Principal,
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private eventManager: JhiEventManager
+    ) {
+        this.itemsPerPage = ITEMS_PER_PAGE;
+        this.routeData = this.activatedRoute.data.subscribe(data => {
+            this.page = data.pagingParams.page;
+            this.previousPage = data.pagingParams.page;
+            this.reverse = data.pagingParams.ascending;
+            this.predicate = data.pagingParams.predicate;
+        });
+    }
+
+    loadAll() {
+        this.threadService
+            .query({
+                page: this.page - 1,
+                size: this.itemsPerPage,
+                sort: this.sort()
+            })
+            .subscribe(
+                (res: HttpResponse<IThread[]>) => this.paginateThreads(res.body, res.headers),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
+
+    loadPage(page: number) {
+        if (page !== this.previousPage) {
+            this.previousPage = page;
+            this.transition();
         }
+    }
+
+    transition() {
+        this.router.navigate(['/thread'], {
+            queryParams: {
+                page: this.page,
+                size: this.itemsPerPage,
+                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+            }
+        });
+        this.loadAll();
+    }
+
+    clear() {
+        this.page = 0;
+        this.router.navigate([
+            '/thread',
+            {
+                page: this.page,
+                sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+            }
+        ]);
+        this.loadAll();
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInThreads();
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: IThread) {
+        return item.id;
+    }
+
+    registerChangeInThreads() {
+        this.eventSubscriber = this.eventManager.subscribe('threadListModification', response => this.loadAll());
+    }
+
+    sort() {
+        const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+        if (this.predicate !== 'id') {
+            result.push('id');
+        }
+        return result;
+    }
+
+    private paginateThreads(data: IThread[], headers: HttpHeaders) {
+        this.links = this.parseLinks.parse(headers.get('link'));
+        this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+        this.queryCount = this.totalItems;
+        this.threads = data;
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 
     onChipClick() {
@@ -22,13 +138,5 @@ export class ForumComponent {
             height: '75%',
             data: 0
         });
-    }
-}
-
-class Thread {
-    public readonly imageUrl: String;
-
-    constructor(imageName: String, public name: String, public description: String) {
-        this.imageUrl = '../../content/images/weapons/' + imageName + '.jpg';
     }
 }
--- src/main/webapp/app/forum/forum.route.ts
+++ src/main/webapp/app/forum/forum.route.ts
@@ -1,12 +1,19 @@
 import { Route } from '@angular/router';
 
 import { ForumComponent } from './';
+import { JhiResolvePagingParams } from 'ng-jhipster';
+import { UserRouteAccessService } from 'app/core';
 
 export const FORUM_ROUTE: Route = {
     path: 'forum',
     component: ForumComponent,
     data: {
         authorities: [],
-        pageTitle: 'Welcome, Java Hipster!'
-    }
+        defaultSort: 'id,asc',
+        pageTitle: 'Foro!'
+    },
+    resolve: {
+        pagingParams: JhiResolvePagingParams
+    },
+    canActivate: [UserRouteAccessService]
 };
