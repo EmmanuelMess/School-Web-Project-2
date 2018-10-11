70d02c2ae0994a57595878ada3501f0df98fd7af
Emmanuel M <emmanuelbendavid@gmail.com>
2018-10-11 20:31:22 -0300

    Added close button to message component

--- src/main/webapp/app/forum/message/message.component.ts
+++ src/main/webapp/app/forum/message/message.component.ts
@@ -4,7 +4,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 @Component({
     selector: 'jhi-message',
     templateUrl: './message.component.html',
-    styles: []
+    styleUrls: ['message.scss']
 })
 export class MessageComponent {
     constructor(public dialogRef: MatDialogRef<MessageComponent>, @Inject(MAT_DIALOG_DATA) public data: number) {}
